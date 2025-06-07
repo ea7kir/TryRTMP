cf1cf20aede94c7a3501218ca245b2019f82be92
Michael Naylor <mikenaylorspain@icloud.com>
2025-06-07 15:10:33 +0200

    edit June 7 - 15:11

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
-# Not working - yet
+# Not working - yet - June 7 - 15:11
+My mission is to get this working before replacing `videotestsrc` with `appsrc`.
 
+This would be a major step forward and allow further testing before moving on.
+
+To help debug this I've 2 approaches.
+
+## Line 125 contains an `if statement` to allow 2 ways to be tested.
+### 1. using `pipeline.Add`
 ```
 $ go run .
-2025/06/05 12:50:50 main.go:226: TryRTMP -> rtmp://somewhere.any/live/name
-2025/06/05 12:50:50 main.go:39: TryRTMP start creating pipeline
-2025/06/05 12:50:50 main.go:91: TryRTMP end of ElementFactoryMake
-2025/06/05 12:50:50 main.go:157: TryRTMP failed to add capsfilter2
-2025/06/05 12:50:50 main.go:166: TryRTMP failed to add flvmux
+2025/06/07 14:11:55 main.go:256: TryRTMP -> rtmp://somewhere.any/live/name
+2025/06/07 14:11:55 main.go:34: TryRTMP start creating pipeline
+2025/06/07 14:11:55 main.go:86: TryRTMP end of ElementFactoryMake
+2025/06/07 14:11:55 main.go:126: TryRTMP using pipeline.Add
+0:00:00.017550848 240193     0x358b1b00 WARN                     bin gstbin.c:1381:gst_bin_add_func:<pipeline0> Name 'capsfilter2x' is not unique in bin, not adding
+2025/06/07 14:11:55 main.go:152: TryRTMP failed to add capsfilter2x
+0:00:00.017592674 240193     0x358b1b00 WARN                     bin gstbin.c:1381:gst_bin_add_func:<pipeline0> Name 'mux' is not unique in bin, not adding
+2025/06/07 14:11:55 main.go:161: TryRTMP failed to add flvmux
+2025/06/07 14:11:55 main.go:206: TryRTMP fatal: gst.LinkMany returned false
 exit status 1
 ```
+### 2. using `pipeline.AddMany`
+```
+$ go run .
+2025/06/07 14:13:03 main.go:34: TryRTMP start creating pipeline
+2025/06/07 14:13:03 main.go:86: TryRTMP end of ElementFactoryMake
+2025/06/07 14:13:03 main.go:165: TryRTMP using pipeline.AddMany
+0:00:00.013906352 240660      0xde0fb00 WARN                     bin gstbin.c:1381:gst_bin_add_func:<pipeline0> Name 'mux' is not unique in bin, not adding
+2025/06/07 14:13:03 main.go:169: TryRTMP fatal: pipeline.AddMany returned false
+exit status 1
+```
+## Notes
+`gst.LinkMany` has been editedto how I interpret @RSWilli's suggestion.
+
+Line 210 exits, because I'm using a summy destination url.
--- main.go
+++ main.go
@@ -28,13 +28,7 @@ gst-launch-1.0 \
     ! mux.
 */
 
-const RTMP_DEST = "rtmp://somewhere.any/live/name"
-
-// func fatalIfNill(v any, msg string) {
-// 	if v == nil {
-// 		panic(msg)
-// 	}
-// }
+const RTMP_DEST = "rtmp://somewhere.any/live/name" // i'm unable to reveal the real url to the pubic
 
 func createPipeline() (gst.Pipeline, error) {
 	log.Printf("TryRTMP start creating pipeline")
@@ -58,34 +52,34 @@ func createPipeline() (gst.Pipeline, error) {
 
 	pipeline := gst.NewPipeline("").(gst.Pipeline)
 
-	if videotestsrc = gst.ElementFactoryMake("videotestsrc", ""); videotestsrc == nil {
+	if videotestsrc = gst.ElementFactoryMake("videotestsrc", "videotestsrc"); videotestsrc == nil {
 		log.Printf("TryRTMP videotestsrc is nil")
 	}
-	if videoconvert = gst.ElementFactoryMake("videoconvert", ""); videoconvert == nil {
+	if videoconvert = gst.ElementFactoryMake("videoconvert", "videoconvert"); videoconvert == nil {
 		log.Printf("TryRTMP videoconvert is nil")
 	}
-	if capsfilter1 = gst.ElementFactoryMake("capsfilter", ""); capsfilter1 == nil {
+	if capsfilter1 = gst.ElementFactoryMake("capsfilter", "capsfilter1"); capsfilter1 == nil {
 		log.Printf("TryRTMP capsfilter1 is nil")
 	}
-	if queue = gst.ElementFactoryMake("queue", ""); queue == nil {
+	if queue = gst.ElementFactoryMake("queue", "queue"); queue == nil {
 		log.Printf("TryRTMP queue is nil")
 	}
-	if x264enc = gst.ElementFactoryMake("x264enc", ""); x264enc == nil {
+	if x264enc = gst.ElementFactoryMake("x264enc", "x264enc"); x264enc == nil {
 		log.Printf("TryRTMP x264enc is nil")
 	}
-	if capsfilter2 = gst.ElementFactoryMake("capsfilter", ""); capsfilter2 == nil {
+	if capsfilter2 = gst.ElementFactoryMake("capsfilter", "capsfilter2"); capsfilter2 == nil {
 		log.Printf("TryRTMP capsfilter2 is nil")
 	}
-	if flvmux = gst.ElementFactoryMake("flvmux", ""); flvmux == nil {
+	if flvmux = gst.ElementFactoryMake("flvmux", "mux"); flvmux == nil {
 		log.Printf("TryRTMP flvmux is nil")
 	}
-	if rtmpsink = gst.ElementFactoryMake("rtmpsink", ""); rtmpsink == nil {
+	if rtmpsink = gst.ElementFactoryMake("rtmpsink", "rtmpsink"); rtmpsink == nil {
 		log.Printf("TryRTMP rtmpsink is nil")
 	}
-	if audiotestsrc = gst.ElementFactoryMake("audiotestsrc", ""); audiotestsrc == nil {
+	if audiotestsrc = gst.ElementFactoryMake("audiotestsrc", "audiotestsrc"); audiotestsrc == nil {
 		log.Printf("TryRTMP audiotestsrc is nil")
 	}
-	if voaacenc = gst.ElementFactoryMake("voaacenc", ""); voaacenc == nil {
+	if voaacenc = gst.ElementFactoryMake("voaacenc", "voaacenc"); voaacenc == nil {
 		log.Printf("TryRTMP voaacenc is nil")
 	}
 
@@ -128,62 +122,92 @@ func createPipeline() (gst.Pipeline, error) {
 
 	// Add the elements to the pipeline and link them
 
-	// add one at a time to finf the offending element
-
-	if ok := pipeline.Add(videotestsrc); ok != true {
-		log.Printf("TryRTMP failed to add videotestsrc")
-	}
-	if ok := pipeline.Add(videoconvert); ok != true {
-		log.Printf("TryRTMP failed to add videoconvert")
-	}
-	if ok := pipeline.Add(capsfilter1); ok != true {
-		log.Printf("TryRTMP failed to add capsfilter1")
-	}
-	if ok := pipeline.Add(queue); ok != true {
-		log.Printf("TryRTMP failed to add queue")
-	}
-	if ok := pipeline.Add(x264enc); ok != true {
-		log.Printf("TryRTMP failed to add x264enc")
-	}
-	if ok := pipeline.Add(capsfilter2); ok != true {
-		log.Printf("TryRTMP failed to add capsfilter2")
-	}
-	if ok := pipeline.Add(flvmux); ok != true {
-		log.Printf("TryRTMP failed to add flvmux")
-	}
-	if ok := pipeline.Add(rtmpsink); ok != true {
-		log.Printf("TryRTMP failed to add rtmpsink")
-	}
-	if ok := pipeline.Add(capsfilter2); ok != true {
-		log.Printf("TryRTMP failed to add capsfilter2")
-	}
-	if ok := pipeline.Add(audiotestsrc); ok != true {
-		log.Printf("TryRTMP failed to add audiotestsrc")
-	}
-	if ok := pipeline.Add(voaacenc); ok != true {
-		log.Printf("TryRTMP failed to add voaacenc")
-	}
-	if ok := pipeline.Add(flvmux); ok != true {
-		log.Printf("TryRTMP failed to add flvmux")
-	}
-
-	os.Exit(1)
-
-	if ok := pipeline.AddMany(videotestsrc, videoconvert, capsfilter1,
-		queue, x264enc, capsfilter2,
-		flvmux, rtmpsink, audiotestsrc, voaacenc, flvmux); ok != true {
-		log.Fatalf("TryRTMP fatal: pipeline.AddMany returned %v", ok)
+	if true {
+		log.Printf("TryRTMP using pipeline.Add")
+		if ok := pipeline.Add(videotestsrc); ok != true {
+			log.Printf("TryRTMP failed to add videotestsrc")
+		}
+		if ok := pipeline.Add(videoconvert); ok != true {
+			log.Printf("TryRTMP failed to add videoconvert")
+		}
+		if ok := pipeline.Add(capsfilter1); ok != true {
+			log.Printf("TryRTMP failed to add capsfilter1")
+		}
+		if ok := pipeline.Add(queue); ok != true {
+			log.Printf("TryRTMP failed to add queue")
+		}
+		if ok := pipeline.Add(x264enc); ok != true {
+			log.Printf("TryRTMP failed to add x264enc")
+		}
+		if ok := pipeline.Add(capsfilter2); ok != true {
+			log.Printf("TryRTMP failed to add capsfilter2")
+		}
+		if ok := pipeline.Add(flvmux); ok != true {
+			log.Printf("TryRTMP failed to add flvmux")
+		}
+		if ok := pipeline.Add(rtmpsink); ok != true {
+			log.Printf("TryRTMP failed to add rtmpsink")
+		}
+		if ok := pipeline.Add(capsfilter2); ok != true {
+			log.Printf("TryRTMP failed to add capsfilter2")
+		}
+		if ok := pipeline.Add(audiotestsrc); ok != true {
+			log.Printf("TryRTMP failed to add audiotestsrc")
+		}
+		if ok := pipeline.Add(voaacenc); ok != true {
+			log.Printf("TryRTMP failed to add voaacenc")
+		}
+		if ok := pipeline.Add(flvmux); ok != true {
+			log.Printf("TryRTMP failed to add flvmux")
+		}
+		// os.Exit(1)
+	} else {
+		log.Printf("TryRTMP using pipeline.AddMany")
+		if ok := pipeline.AddMany(videotestsrc, videoconvert, capsfilter1,
+			queue, x264enc, capsfilter2,
+			flvmux, rtmpsink, audiotestsrc, voaacenc, flvmux); ok != true {
+			log.Fatalf("TryRTMP fatal: pipeline.AddMany returned %v", ok)
+		}
+		// os.Exit(1)
 	}
 
 	// TODO: need to call Link (or LinkMany) for each “!” in the gst-launch call
+	/*
+		y@RSWilli sya I'm calling pipeline.LinkMany which will fail, because this call will link
+		all elements in the order they were passed. My gst-launch pipeline is linked
+		differently. I need to call Link (or LinkMany) for each “!” in your gst-launch call.
 
-	if ok := gst.LinkMany(videotestsrc, videoconvert, capsfilter1,
-		queue, x264enc, capsfilter2,
-		flvmux, rtmpsink, audiotestsrc, voaacenc, flvmux); ok != true {
+		gst-launch-1.0 \
+		    videotestsrc is-live=1 \
+		    ! videoconvert \
+		    ! "video/x-raw, width=1920, height=1080, framerate=25/1" \
+		    ! queue \
+		    ! x264enc cabac=1 bframes=2 ref=1 \
+		    ! "video/x-h264,profile=main" \
+		    ! flvmux streamable=true name=mux \
+		    ! rtmpsink location="${RTMP_DEST} live=1" \
+		    audiotestsrc is-live=1 wave=ticks \
+		    ! voaacenc bitrate=128000 \
+		    ! mux.
+	*/
+
+	if ok := gst.LinkMany(
+		// videotestsrc,
+		videoconvert,
+		capsfilter1,
+		queue,
+		x264enc,
+		capsfilter2,
+		flvmux,
+		rtmpsink,
+		// audiotestsrc,
+		voaacenc,
+		flvmux); ok != true {
 		log.Fatalf("TryRTMP fatal: gst.LinkMany returned %v", ok)
 	}
 
 	log.Printf("TryRTMP end of creating pipeline")
+	os.Exit(1) // no point proceeding without a real destination url
 	return pipeline, nil
 }
 
